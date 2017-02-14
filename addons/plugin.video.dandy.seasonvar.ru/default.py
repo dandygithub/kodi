eff373007e3e763b7bdc95d54f129965415df4c1
dandygithub <denis.bolotin.dandy@gmail.com>
2017-02-14 12:42:26 +0300

    modif seasonvar addon
    + login
    + type begin list

--- addons/plugin.video.dandy.seasonvar.ru/default.py
+++ addons/plugin.video.dandy.seasonvar.ru/default.py
@@ -37,6 +37,7 @@ except:
 
 
 class Seasonvar():
+
     def __init__(self):
         self.id = 'plugin.video.dandy.seasonvar.ru'
         self.addon = xbmcaddon.Addon(self.id)
@@ -53,6 +54,19 @@ class Seasonvar():
 
         self.inext = os.path.join(self.path, 'resources/icons/next.png')
         self.debug = False
+
+        self.begin_render_type = self.addon.getSetting('begin_render_type') if self.addon.getSetting('begin_render_type') else None        
+        
+        self.login()
+
+    def login(self):
+        print "*** Login"
+        
+        login = self.addon.getSetting('login')
+        if login:
+            password = self.addon.getSetting('password')
+            url = self.url + '/?mod=login&login=' + unicode(login) + '&password=' + unicode(password)
+            response = common.fetchPage({"link": url})            
 
     def main(self):
         self.log("Addon: %s"  % self.id)
@@ -73,6 +87,8 @@ class Seasonvar():
             self.search(keyword, unified)
         if mode == 'show':
             self.getFilmInfo(url, (params['wm'] == "1"))
+        if mode == 'filter':
+            self.filter()
         elif mode is None:
             self.menu()
 
@@ -81,32 +97,40 @@ class Seasonvar():
         item = xbmcgui.ListItem("[COLOR=FF00FF00]%s[/COLOR]" % self.language(2000), thumbnailImage=self.icon)
         xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
 
-        self.getItems(self.url, 0)
+        self.getItems(self.url)
 
         xbmc.executebuiltin('Container.SetViewMode(52)')
         xbmcplugin.endOfDirectory(self.handle, True)
 
-    def getItems(self, url, kind):
+    def getItems(self, url):
         print "*** Get items from %s" % url
         url_ = "%s/" % (url)
-        response = None
-        if kind == 0:
+        content = None
+
+        if (self.begin_render_type == None) or (self.begin_render_type == 'new'):
             url_ = url_ + 'ajax.php?mode=new'
-            values = {}
-            headers = {
-                "Host" : "seasonvar.ru",
-                "Connection" : "keep-alive",
-                "X-Requested-With" : "XMLHttpRequest",
-                "Referer" : url_,
-                "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:35.0) Gecko/20100101 Firefox/35.0"
-            }
-            request = urllib2.Request(url_, urllib.urlencode(values), headers)
-            response = urllib2.urlopen(request).read()            
+        elif (self.begin_render_type == 'popular'):
+            url_ = url_ + 'ajax.php?mode=pop'
+        elif (self.begin_render_type == 'newest'):
+            url_ = url_ + 'ajax.php?mode=newest'
+        else:
+            url_ = url_ + 'ajax.php?mode=new'
+            
+        values = {}
+        headers = {
+            "Host" : "seasonvar.ru",
+            "Connection" : "keep-alive",
+            "X-Requested-With" : "XMLHttpRequest",
+            "Referer" : url_,
+            "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:35.0) Gecko/20100101 Firefox/35.0"
+        }
+        request = urllib2.Request(url_, urllib.urlencode(values), headers)
+        content = urllib2.urlopen(request).read()            
             
         #response = common.fetchPage({"link": page_url})
 
-        if response:
-            items = common.parseDOM(response, "div", attrs={"class": "news-block"})
+        if content:
+            items = common.parseDOM(content, "div", attrs={"class": "news-block"})
             for item in items:
                 titlediv = common.parseDOM(item, "div", attrs={"class": "news-right"})
                 title = common.parseDOM(titlediv, "b")[0]
@@ -143,7 +167,8 @@ class Seasonvar():
         print "*** getFilmInfo for url %s " % url
 
         response = common.fetchPage({"link": url})
-        image = common.parseDOM(response["content"], 'link', attrs={'rel': 'image_src'}, ret='href')[0]        
+        image = common.parseDOM(response["content"], 'link', attrs={'rel': 'image_src'}, ret='href')[0] if common.parseDOM(response["content"], 'link', attrs={'rel': 'image_src'}, ret='href') else None
+        description = common.parseDOM(response["content"], 'meta', attrs={'name': 'description'}, ret='content')[0] if common.parseDOM(response["content"], 'meta', attrs={'name': 'description'}, ret='content') else None
         multiseason = common.parseDOM(response["content"], 'div', attrs={'class': 'svtabr_wrap show seasonlist'})
 
         if (not withoutmulti) and multiseason:
@@ -154,24 +179,36 @@ class Seasonvar():
                 title = title.replace('<span>', '[COLOR=FF00FFF0][').replace('</span>', '][/COLOR]').replace('<small>', '[COLOR=FF00FFF0][').replace('</small>', '][/COLOR]')
                 title = ' '.join(title.split()).strip()
                 uri = sys.argv[0] + '?mode=show&url=%s&wm=1' % url
-                item = xbmcgui.ListItem(title, iconImage=image, thumbnailImage=image)
+                item = xbmcgui.ListItem(self.strip(title), iconImage=image, thumbnailImage=image)
                 item.setInfo(type='Video', infoLabels={'title': title})
+                item.select(title.find('>>>') > -1)
                 xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
            
         else:
+            bad = common.parseDOM(response["content"], 'div', attrs={'class': 'sobadcat'})
+            if bad:
+                self.showErrorMessage(self.strip(bad[0]))
+                exit
             divplayer = common.parseDOM(response["content"], 'div', attrs={'id': 'videoplayer719'})
-            jshash = common.parseDOM(divplayer, "script", attrs={"type": "text/javascript"})[0]
-            uhash = jshash.split('var pl0 = "')[-1].split('";')[0] if 'var pl0 = "' in jshash else None
+            hashdiv = common.parseDOM(divplayer, "script", attrs={"type": "text/javascript"})[0]
+            uhash = hashdiv.split('var pl0 = "')[-1].split('";')[0] if 'var pl0 = "' in hashdiv else None
             pl_url = Decoder.Decode_String(uhash, 'JpvnsR03Tmwu9xgaGLUXztb7H=' + '\n' + 'fNW5elVDyZIiMoQ1B826cd4YkC')
             response = common.fetchPage({"link": self.url + pl_url})
             json_playlist = json.loads(response["content"])
             playlist = json_playlist['playlist']
+            if not playlist:
+                hashdiv = common.parseDOM(divplayer, "div", attrs={"id": "translateDivParent"})[0]
+                uhash = hashdiv.split('var pl68 = "')[-1].split('";')[0] if 'var pl68 = "' in hashdiv else None
+                pl_url = Decoder.Decode_String(uhash, 'JpvnsR03Tmwu9xgaGLUXztb7H=' + '\n' + 'fNW5elVDyZIiMoQ1B826cd4YkC')                
+                response = common.fetchPage({"link": self.url + pl_url})
+                json_playlist = json.loads(response["content"])
+                playlist = json_playlist['playlist']
             for episode in playlist:
                 etitle = self.strip(episode['comment'])
                 url = episode['file']
                 uri = sys.argv[0] + '?mode=play&url=%s' % url
-                item = xbmcgui.ListItem(common.stripTags(etitle), iconImage=image, thumbnailImage=image)
-                labels = {'title': etitle, 'overlay': xbmcgui.ICON_OVERLAY_WATCHED, 'playCount': 0}
+                item = xbmcgui.ListItem(etitle, iconImage=image, thumbnailImage=image)
+                labels = {'title': description if description != None else etitle, 'overlay': xbmcgui.ICON_OVERLAY_WATCHED, 'playCount': 0}
                 item.setInfo(type='Video', infoLabels=labels)
                 item.setProperty('IsPlayable', 'true')
                 xbmcplugin.addDirectoryItem(self.handle, uri, item, False)
@@ -260,6 +297,9 @@ class Seasonvar():
 
         else:
             self.menu()
+
+    def filter(self):
+        exit
 
     # *** Add-on helpers
     def log(self, message):
