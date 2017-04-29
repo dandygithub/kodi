e21bde6e6512f31325e78923014ff2d9319a92fc
dandygithub <denis.bolotin.dandy@gmail.com>
2017-04-29 17:09:56 +0300

    update media info addon for tv

--- addons/context.dandy.mediainfo/default.py
+++ addons/context.dandy.mediainfo/default.py
@@ -16,7 +16,8 @@ import TheMovieDB as tmdb
 ID = "context.dandy.mediainfo"
 ADDON = xbmcaddon.Addon(ID)
 
-PATTERNS_FOR_EDIT = ADDON.getSetting('patterns_edit') if ADDON.getSetting('patterns_edit') else r"(.*)"
+PATTERNS_FOR_EDIT = ADDON.getSetting('patterns_edit') if ADDON.getSetting('patterns_edit') else ".*"
+PATTERNS_FOR_DELETE = ADDON.getSetting('patterns_delete') if ADDON.getSetting('patterns_delete') else "[(].+?[)],[[].+?[]]"
 
 _title_ = ""
 _year_ = ""
@@ -31,7 +32,7 @@ def check_is_edit(title):
         patterns = PATTERNS_FOR_EDIT.split(",") if PATTERNS_FOR_EDIT else []
         isedit = False
         for pattern in patterns:
-            match = re.match(pattern, title)
+            match = re.compile(pattern.decode('utf-8')).match(title.decode('utf-8'))
             if match:
                isedit = True
                break
@@ -51,17 +52,17 @@ def edit_title(title):
 
 def get_media_title():
     title = get_title()
-    patterns = [r"[(].+?[)]", r"[[].+?[]]"]
+    patterns = PATTERNS_FOR_DELETE.split(",") if PATTERNS_FOR_DELETE else []
     for pattern in patterns:
-        title = re.sub(pattern, "", title).strip()
-    title = title.split("/")[0].strip()
+        title = re.compile(pattern.decode('utf-8')).sub("", title.decode('utf-8')).strip()
+    #title = title.split("/")[0].strip()
     title = edit_title(title) 
     return title
 
 def get_media_meta_year():
     title = get_title()
     pattern = r"[([]([12][90]\d\d)[]), ]"
-    match = re.search(pattern, title)
+    match = re.compile(pattern.decode('utf-8')).search(title.decode('utf-8'))
     return match.group(1) if match else ""
 
 def get_date_by_tag(item):
@@ -74,7 +75,7 @@ def select_media(data):
     global _year_
     media = []
     for item in data:
-        media.append("%s [%s]" % (item["title"], get_date_by_tag(item)))
+        media.append("%s/%s [%s]" % (item["title"], item["original_title"], get_date_by_tag(item)))
 #tmdb={u'total_results': 1, u'total_pages': 1, u'page': 1, u'results': [{u'poster_path': u'/jB5VQeYO2nr4h31RjXnnER9JoRN.jpg', u'title': u'\u0421\u0444\u0435\u0440\u0430', u'overview': u'\u0424\u0438\u043b\u044c\u043c \u0440\u0430\u0441\u0441\u043a\u0430\u0436\u0435\u0442 \u043e\u0431 \u0438\u043d\u0442\u0435\u0440\u043d\u0435\u0442-\u043a\u043e\u043c\u043f\u0430\u043d\u0438\u0438 The Circle, \u043e\u0431\u044a\u0435\u0434\u0438\u043d\u044f\u044e\u0449\u0435\u0439 \u0432 \u0435\u0434\u0438\u043d\u0443\u044e \u0441\u0438\u0441\u0442\u0435\u043c\u0443 \u044d\u043b\u0435\u043a\u0442\u0440\u043e\u043d\u043d\u0443\u044e \u043f\u043e\u0447\u0442\u0443 \u0441\u0432\u043e\u0438\u0445 \u043f\u043e\u043b\u044c\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u0435\u0439, \u0441\u0442\u0440\u0430\u043d\u0438\u0447\u043a\u0438 \u0432 \u0441\u043e\u0446\u0438\u0430\u043b\u044c\u043d\u044b\u0445 \u0441\u0435\u0442\u044f\u0445, \u0438\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u044e \u043e \u0431\u0430\u043d\u043a\u043e\u0432\u0441\u043a\u0438\u0445 \u043a\u0430\u0440\u0442\u0430\u0445 \u0438 \u043f\u043e\u043a\u0443\u043f\u043a\u0430\u0445. \u0420\u0435\u0437\u0443\u043b\u044c\u0442\u0430\u0442\u043e\u043c \u044d\u0442\u043e\u0433\u043e \u043f\u0440\u043e\u0446\u0435\u0441\u0441\u0430 \u0441\u0442\u0430\u043d\u043e\u0432\u0438\u0442\u0441\u044f \u043f\u043e\u043b\u0443\u0447\u0435\u043d\u0438\u0435 \u0447\u0435\u043b\u043e\u0432\u0435\u043a\u043e\u043c \u0443\u043d\u0438\u0432\u0435\u0440\u0441\u0430\u043b\u044c\u043d\u043e\u0433\u043e \u043e\u043d\u043b\u0430\u0439\u043d-\u0438\u0434\u0435\u043d\u0442\u0438\u0444\u0438\u043a\u0430\u0442\u043e\u0440\u0430. \u041d\u043e \u043d\u0430 \u043f\u0440\u0430\u043a\u0442\u0438\u043a\u0435 \u043d\u0435 \u0432\u0441\u0435 \u0442\u0430\u043a \u0433\u043b\u0430\u0434\u043a\u043e: \u043f\u0440\u043e\u0435\u043a\u0442, \u043f\u043e\u0437\u0438\u0446\u0438\u043e\u043d\u0438\u0440\u0443\u0435\u043c\u044b\u0439 \u043a\u0430\u043a \u043d\u043e\u0432\u0430\u044f \u0441\u0442\u0443\u043f\u0435\u043d\u044c \u0432 \u0440\u0430\u0437\u0432\u0438\u0442\u0438\u0438 \u0442\u0435\u0445\u043d\u043e\u043b\u043e\u0433\u0438\u0439, \u043f\u0440\u0435\u0441\u043b\u0435\u0434\u0443\u0435\u0442 \u0438 \u0434\u0440\u0443\u0433\u0438\u0435, \u0431\u043e\u043b\u0435\u0435 \u0433\u0440\u044f\u0437\u043d\u044b\u0435 \u0446\u0435\u043b\u0438.', u'release_date': u'2017-04-27', u'popularity': 9.357582, u'original_title': u'The Circle', u'backdrop_path': u'/mYpuI61SFcFD9UCkJOLGVxZkIfz.jpg', u'vote_count': 3, u'video': False, u'adult': False, u'vote_average': 0.5, u'original_language': u'en', u'id': 339988, u'genre_ids': [18, 53, 878]}]}
     ret = 0
     if len(media) > 0:
@@ -105,7 +106,7 @@ def get_media_meta_movie_id():
     if _title_ == "":
         return ""
     global _year_
-    params = {"query": "%s" % _title_,
+    params = {"query": _title_,
               "language": "ru",
               "include_adult": True}
     if _year_ != "":
