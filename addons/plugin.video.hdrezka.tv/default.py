c70525705f40af61f9a175c59bbabf973b9635c7
dandygithub <denis.bolotin.dandy@gmail.com>
2017-08-28 09:57:11 +0300

    repair hdrezka addon
    repair hdrezka addon

--- addons/plugin.video.hdrezka.tv/default.py
+++ addons/plugin.video.hdrezka.tv/default.py
@@ -342,7 +342,8 @@ class HdrezkaTV():
 
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-            "Referer": mainurl
+#            "Referer": mainurl
+            "Referer": "http://google.com"
         }
         request = urllib2.Request(url, "", headers)
         request.get_method = lambda: 'GET'
