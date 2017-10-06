6ec3ee63f6f53777b58e478ac2106be9d564c146
dandygithub <denis.bolotin.dandy@gmail.com>
2017-10-07 00:51:13 +0300

    repair moonwalk

--- addons/script.module.videohosts/lib/videohosts/moonwalk.py
+++ addons/script.module.videohosts/lib/videohosts/moonwalk.py
@@ -1,13 +1,18 @@
 # coding: utf-8
 # Author: dandy
 
+import urllib, urllib2
+import xbmc
+import XbmcHelpers
+common = XbmcHelpers
+
 def get_key(content):
     key = ''
     value = '' 
     try:  
-        script = content.split("setTimeout(function()")[-1].split("</script>")[0]    
-        key = script.split("']['")[-1].split("'] = '")[0].replace("' + '", "")
-        value = script.split("'] = '")[-1].split("';")[0].replace("' + '", "")
+        data = content.split('mw_key:"')[-1].split('",mw_pid')[0]
+        key = data.split('",')[-1].split(':"')[0]
+        value = data.split(':"')[-1]
     except:
         pass 
     return key, value
@@ -17,19 +22,27 @@ def get_access_attrs(content):
     values = {}
     attrs = {}
 
-    attrs['purl'] = '/manifests' + content.split("'/manifests")[-1].split("';")[0]
+    script = "http://s4.cdnapponline.com" + common.parseDOM(content, "script", ret="src")[0]
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+    }
+    request = urllib2.Request(script, "", headers)
+    request.get_method = lambda: 'GET'
+    response = urllib2.urlopen(request).read()
 
-    values['mw_key'] = content.split("var mw_key = '")[-1].split("';")[0] 
-    values['video_token'] = content.split("video_token: '")[-1].split("',")[0] 
-    values['mw_pid'] = content.split("mw_pid: ")[-1].split(",")[0] 
-    values['p_domain_id'] = content.split("p_domain_id: ")[-1].split(",")[0] 
-    values['content_type'] = content.split("content_type: '")[-1].split("',")[0],
-    values['ad_attr'] = '0'
+    attrs['purl'] = '/manifests/video/' + content.split("video_token: '")[-1].split("',")[0]  + "/all"
 
-    key, value = get_key(content) 
+    values['mw_key'] = response.split('mw_key:"')[-1].split('",')[0] 
+    values['ad_attr'] = response.split('ad_attr:')[-1].split(',')[0] 
+    values['iframe_version'] = response.split('iframe_version:')[-1].split('}')[0] 
+
+    values['p_domain_id'] = content.split("domain_id: ")[-1].split(",")[0] 
+    values['mw_pid'] = content.split("partner_id: ")[-1].split(",")[0] 
+
+    key, value = get_key(response) 
 
     values[key] = value
 
-    attrs['X-Access-Level'] = content.split("'X-Access-Level': '")[-1].split("'")[0]
+    attrs['X-Access-Level'] = content.split("user_token: '")[-1].split("',")[0] 
 
     return values, attrs
