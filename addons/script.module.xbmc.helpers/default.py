d5903f65f325a549dda2c16d8bb1e26c828a4f8a
dandygithub <denis.bolotin.dandy@gmail.com>
2018-02-04 13:35:04 +0300

    add helpers addon

---
+++ addons/script.module.xbmc.helpers/default.py
@@ -0,0 +1,9 @@
+import sys
+import xbmc
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+
+settings = xbmcaddon.Addon(id='script.module.xbmc.helpers')
+version = "2.1.0"
+plugin = "Translit-" + version
