00c3483ec8849179ba630171acd0564807fe7407
dandygithub <denis.bolotin.dandy@gmail.com>
2019-03-16 17:58:20 +0300

    ksc off czx.to

--- addons/context.dandy.kinopoisk.sc/resources/lib/search.py
+++ addons/context.dandy.kinopoisk.sc/resources/lib/search.py
@@ -5,16 +5,16 @@
 #import sc_videoframe
 # import sc_getmovie
 #import sc_hdnow
-import sc_czxto
+#import sc_czxto
 import sc_yohoho
 
 def process(kp_id):
     list_li = []
-#    list_li += sc_hdgo.process(kp_id)
-    list_li += sc_czxto.process(kp_id)    
     list_li += sc_yohoho.process(kp_id)     
 
 # Archive    
+#    list_li += sc_czxto.process(kp_id)
+#    list_li += sc_hdgo.process(kp_id)
 #    list_li += sc_noughth.process(kp_id)
 #    list_li += sc_moonwalkco.process(kp_id)
 #    list_li += sc_kodik.process(kp_id)
