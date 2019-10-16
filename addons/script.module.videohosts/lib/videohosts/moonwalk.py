2fca07a26d5bea7e4f746f491a0b72d8c9d8d085
Gennady Sadchikov <dessolo@mail.ru>
2019-10-16 17:23:34 +0400

    Fix videohosts by val1ant (http://xbmc.ru/forum/showpost.php?p=142596&postcount=818)

--- addons/script.module.videohosts/lib/videohosts/moonwalk.py
+++ addons/script.module.videohosts/lib/videohosts/moonwalk.py
@@ -6,6 +6,7 @@ import xbmc
 import xbmcaddon
 import XbmcHelpers
 import socket
+
 common = XbmcHelpers
 
 import re
@@ -20,7 +21,8 @@ socket.setdefaulttimeout(120)
 
 id = 'script.module.videohosts'
 addon = xbmcaddon.Addon(id)
-vurl =  addon.getSetting('vurl')
+vurl = addon.getSetting('vurl')
+
 
 class EncryptedData:
     def __init__(self):
@@ -29,45 +31,48 @@ class EncryptedData:
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, separators=(',', ':'))
 
+
 def reload_values(content, url):
     i = 3
     e_value = None
     n_value = None
-    while (i > 0): 
+    while (i > 0):
         response = common.fetchPage({"link": vurl})
         if response["status"] == 200:
-          data = response["content"]
-          if data and "value1" in data:
-              e_value = data.split("value2=")[0].replace("value1=", "").strip()
-              n_value = data.split("value2=")[1]
-              break  
-        i-=1
+            data = response["content"]
+            if data and "value1" in data:
+                e_value = data.split("value2=")[0].replace("value1=", "").strip()
+                n_value = data.split("value2=")[1]
+                break
+        i -= 1
     if e_value and n_value:
-      xbmc.log("v1=" + repr(e_value) + " v2=" + repr(n_value)) 	
-      addon.setSetting('value1', e_value)
-      addon.setSetting('value2', n_value)
+        xbmc.log("v1=" + repr(e_value) + " v2=" + repr(n_value))
+        addon.setSetting('value1', e_value)
+        addon.setSetting('value2', n_value)
     return get_access_attrs(content, url, False)
 
-#http://wonky.lostcut.net/moonwalk_key.php
+
+# http://wonky.lostcut.net/moonwalk_key.php
 def reload_values2(content, url):
     i = 3
     e_value = None
     n_value = None
-    while (i > 0): 
+    while (i > 0):
         response = common.fetchPage({"link": vurl})
         if response["status"] == 200:
-          data = response["content"]
-          if data and "key" in data:
-              json_data = json.loads(data)
-              e_value = json_data["key"]
-              n_value = json_data["iv"]
-              break  
-        i-=1
+            data = response["content"]
+            if data and "key" in data:
+                json_data = json.loads(data)
+                e_value = json_data["key"]
+                n_value = json_data["iv"]
+                break
+        i -= 1
     if e_value and n_value:
-      xbmc.log("v1=" + repr(e_value) + " v2=" + repr(n_value))    
-      addon.setSetting('value1', e_value)
-      addon.setSetting('value2', n_value)
+        xbmc.log("v1=" + repr(e_value) + " v2=" + repr(n_value))
+        addon.setSetting('value1', e_value)
+        addon.setSetting('value2', n_value)
     return get_access_attrs(content, url, False)
+
 
 def get_cookies(content):
     cookie = re.compile(r"window\[\'(\w*)\'\]\s=\s\'(\w*)\';").findall(content)[0]
@@ -77,6 +82,7 @@ def get_cookies(content):
     cookie_data = re.sub('\'|\s|\+', '', cookie_data)
     cookies = [cookie_header, cookie_data]
     return cookies
+
 
 def get_access_attrs(content, url, check=True):
     values = {}
@@ -95,7 +101,7 @@ def get_access_attrs(content, url, check=True):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
     }
-    request = urllib2.Request("http://" + url.split('/')[2] + js_path, "", headers)
+    request = urllib2.Request(js_path, "", headers)
     request.get_method = lambda: 'GET'
     js_page = urllib2.urlopen(request).read()
 
@@ -103,7 +109,7 @@ def get_access_attrs(content, url, check=True):
     t.a = mw_pid
     t.b = p_domain_id
     t.c = _mw_adb
-    #t.d = window_value
+    # t.d = window_value
     t.e = video_token
     t.f = USER_AGENT
 
@@ -126,10 +132,10 @@ def get_access_attrs(content, url, check=True):
     values["q"] = base64.standard_b64encode(encrypted)
     values["ref"] = ref
 
-#check
+    # check
     if (check == True) and vurl:
         response = ''
-        try: 
+        try:
             opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
             opener.addheaders = [("User-Agent", USER_AGENT)]
             request = urllib2.Request(attrs["purl"], urllib.urlencode(values), {})
@@ -146,14 +152,15 @@ def get_access_attrs(content, url, check=True):
 
 def get_key(content, content2):
     key = ''
-    value = '' 
-    try:  
+    value = ''
+    try:
         data = content.split("window['")[-1].split("';")[0]
         value = data.split("'")[2]
-        key = content2.split('};n.')[-1].split('=e[')[0] 
+        key = content2.split('};n.')[-1].split('=e[')[0]
     except:
-        pass 
+        pass
     return key, value
+
 
 def get_access_attrs_old(content):
     values = {}
@@ -167,16 +174,17 @@ def get_access_attrs_old(content):
     request.get_method = lambda: 'GET'
     response = urllib2.urlopen(request).read()
 
-    attrs['purl'] = '/manifests/video/' + content.split("video_token: '")[-1].split("',")[0]  + "/all"
+    attrs['purl'] = '/manifests/video/' + content.split("video_token: '")[-1].split("',")[0] + "/all"
 
-    values['mw_key'] = response.split('mw_key:"')[-1].split('",')[0] 
-    values['ad_attr'] = response.split('ad_attr:')[-1].split(',')[0] 
-    values['iframe_version'] = response.split('iframe_version:"')[-1].split('"')[0] 
-    values['mw_pid'] = content.split("partner_id: ")[-1].split(",")[0] 
-    values['adb'] = 'false'    
-    
-    values[response.split(',mw_pid:this.options.partner_id,')[-1].split(':this.options.domain_id')[0]] = content.split("domain_id: ")[-1].split(",")[0]     
-    
+    values['mw_key'] = response.split('mw_key:"')[-1].split('",')[0]
+    values['ad_attr'] = response.split('ad_attr:')[-1].split(',')[0]
+    values['iframe_version'] = response.split('iframe_version:"')[-1].split('"')[0]
+    values['mw_pid'] = content.split("partner_id: ")[-1].split(",")[0]
+    values['adb'] = 'false'
+
+    values[response.split(',mw_pid:this.options.partner_id,')[-1].split(':this.options.domain_id')[0]] = \
+    content.split("domain_id: ")[-1].split(",")[0]
+
     param = response.split('t,e.')[-1].split('";var')[0]
     values[param.split('="')[0]] = param.split('="')[1]
 
@@ -184,8 +192,8 @@ def get_access_attrs_old(content):
     value = response.split('"]="')[-1].split('"},loadSecondLayer')[0]
     values[param] = value
 
-    #key, value = get_key(content, response) 
-    #values[key] = value
+    # key, value = get_key(content, response)
+    # values[key] = value
 
     xbmc.log("param=" + repr(values) + " " + repr(attrs))
 
