5a9903674112756bebb0f6cc8c437a614fd3e9ce
dandygithub <denis.bolotin.dandy@gmail.com>
2019-11-18 22:06:15 +0300

    repair hdrezka

--- addons/plugin.video.hdrezka.tv/default.py
+++ addons/plugin.video.hdrezka.tv/default.py
@@ -207,7 +207,7 @@ class HdrezkaTV:
             info = self.get_item_description(post_ids[i])
             title = "%s %s [COLOR=55FFFFFF](%s)[/COLOR]" % (name, color_rating(info['rating']), country_years[i])
             image = self._normalize_url(common.parseDOM(div_covers[i], "img", ret='src')[0])
-            uri = sys.argv[0] + '?mode=show&url=%s' % links[i]
+            uri = sys.argv[0] + '?mode=show&url=%s' % links[i].replace("http:", "https:")
             year, country, genre = get_media_attributes(country_years[i])
             item = xbmcgui.ListItem(title, iconImage=image, thumbnailImage=image)
             item.setInfo(
