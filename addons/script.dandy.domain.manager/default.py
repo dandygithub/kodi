6128d69467fabfa71e656bdb346b1a1138ba01ee
dandygithub <denis.bolotin.dandy@gmail.com>
2019-11-29 23:38:53 +0300

    fix bug

--- addons/script.dandy.domain.manager/default.py
+++ addons/script.dandy.domain.manager/default.py
@@ -13,12 +13,13 @@ common = XbmcHelpers
 
 ID = "script.dandy.domain.manager"
 ADDON = xbmcaddon.Addon(ID)
-PATH = ADDON.getAddonInfo('path')
-HANDLE = int(sys.argv[1])
-PARAMS = sys.argv[2]
-ICON = ADDON.getAddonInfo('icon')
+PATH = ADDON.getAddonInfo("path")
+HANDLE = int(sys.argv[1]) if (len(sys.argv) > 1) else None
+PARAMS = sys.argv[2] if (len(sys.argv) > 2) else None
+ICON = ADDON.getAddonInfo("icon")
 
-SETTINGS = ADDON.getSetting('settings') if ADDON.getSetting('settings') else "domain"
+SETTINGS = ADDON.getSetting("settings") if ADDON.getSetting("settings") else "domain"
+PROTOCOL = ADDON.getSetting("protocol") if ADDON.getSetting("protocol") else None
 
 def show_message(msg):
     xbmc.executebuiltin("XBMC.Notification(%s, %s, %s)" % ("MESSAGE", msg, str(5 * 1000)))
@@ -42,34 +43,43 @@ def get_addons():
     return result.get("addons"), result.get("limits")
 
 def find_domain_setting(addon):
-    domain = None
     setting = None
+    domain = None    
+    protocol = None
+    protocol_value = None
+
     for item in SETTINGS.split(","):
         domain = addon.getSetting(item)
         if domain:
             setting = item
             break 
-    return setting, domain 
+
+    for item in PROTOCOL.split(","):
+        protocol_value = addon.getSetting(item)
+        if protocol_value:
+            protocol = item
+            break 
+
+    return setting, domain, protocol, protocol_value
 
 def menu():
     addons, limits = get_addons() 
     for i, addon in enumerate(addons):
         addon_object = xbmcaddon.Addon(addon['addonid'])
-        setting, domain = find_domain_setting(addon_object)
-        if domain:
+        setting, domain, protocol, protocol_value = find_domain_setting(addon_object)
+        if setting:
             uri = sys.argv[0] + '?mode=%s&addon=%s' % ("edit", addon['addonid'])
-            title = "{0} [COLOR=orange][{1}][/COLOR]".format(addon.get("name"), addon.get("addonid"))
+            title = "{0} [COLOR=orange][{1}][/COLOR]".format(addon.get("name").encode("utf8"), addon.get("addonid"))
             item = xbmcgui.ListItem(title, iconImage=ICON, thumbnailImage=ICON)
             item.setInfo(type='Video', infoLabels={'title': title, 'genre': domain, 'plot': domain})
             xbmcplugin.addDirectoryItem(HANDLE, uri, item, False)
     xbmcplugin.setContent(HANDLE, 'addon')
     xbmcplugin.endOfDirectory(HANDLE, True)
 
-
-def get_user_input(domain):
+def get_user_input(value, title):
     kbd = xbmc.Keyboard()
-    kbd.setDefault(domain)
-    kbd.setHeading("Edit domain")
+    kbd.setDefault(value)
+    kbd.setHeading(title)
     kbd.doModal()
     keyword = None
     if kbd.isConfirmed():
@@ -78,19 +88,24 @@ def get_user_input(domain):
 
 def edit(addon):
     addon_object = xbmcaddon.Addon(addon)
-    setting, domain = find_domain_setting(addon_object)
-    domain = get_user_input(domain)
+    setting, domain, protocol, protocol_value = find_domain_setting(addon_object)
+    if protocol_value:
+        protocol_value = get_user_input(protocol_value, "Edit protocol")
+    domain = get_user_input(domain, "Edit domain")
+    if protocol_value:
+        addon_object.setSetting(protocol, protocol_value)
     if domain:
         addon_object.setSetting(setting, domain)
 
 def main():
-    params = common.getParameters(PARAMS)
-    mode = params['mode'] if 'mode' in params else None
-    addon = params['addon'] if 'addon' in params else None
-    if (mode == "edit"):
-       edit(addon)
-    elif (mode is None):
-       menu()
+    if HANDLE:
+        params = common.getParameters(PARAMS)
+        mode = params['mode'] if 'mode' in params else None
+        addon = params['addon'] if 'addon' in params else None
+        if (mode == "edit"):
+           edit(addon)
+        elif (mode is None):
+           menu()
 
 if __name__ == '__main__':
     main()
