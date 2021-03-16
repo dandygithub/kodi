f3f5b8c22f50e82cc9467601ace629cb43e124e2
dandygithub <denis.bolotin.dandy@gmail.com>
2021-03-16 16:35:40 +0300

    fix bug, coding to utf-8

--- addons/plugin.video.kinokong.net/default.py
+++ addons/plugin.video.kinokong.net/default.py
@@ -45,7 +45,7 @@ class Kinokong():
         self.inext = os.path.join(self.path, 'resources/icons/next.png')
         self.debug = False
         
-        self.news = '/filmy/novinki-2020-godes'
+        self.news = '/filmes/novinki-2020-godes'
 
     def main(self):
         self.log("Addon: %s"  % self.id)
@@ -107,6 +107,7 @@ class Kinokong():
         #print "*** Get category items %s" % url
         page_url = "%s/page/%s/" % (url, str(int(page)))
         response = common.fetchPage({"link": page_url})
+        
         per_page = 0
         pagenav = None
 
