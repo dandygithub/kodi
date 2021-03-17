36d0d53923f5279c89fb06aa37ef21b9f0f9cb07
dandygithub <denis.bolotin.dandy@gmail.com>
2021-03-17 20:07:18 +0300

    remove deprecated addons

--- addons/DEPRECATED/plugin.video.unified.search/unified_search.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# Writer (c) 2012, MrStealth
-# Rev. 1.1.1
-# License: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
-# -*- coding: utf-8 -*-
-
-import resources.lib.unified_search as unified_search 
-
-class UnifiedSearch(unified_search.UnifiedSearch)
