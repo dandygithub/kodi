f567f4afb6962c81ec97211d7cbf453a9c4588d1
mrchnk <mrchnk@gmail.com>
2024-03-10 23:41:20 +0300

    Declare 2k/4k quality types for hdrezka

--- addons/plugin.video.hdrezka.tv/constants.py
+++ addons/plugin.video.hdrezka.tv/constants.py
@@ -1,4 +1,4 @@
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
 PLAYLIST_DOMAIN = 's9.cdnapponline.com'
-QUALITY_TYPES = (360, 480, 720, 1080, 2160)
+QUALITY_TYPES = (360, 480, 720, 1080, 1440, 2160, 2048, 4096)
 PluginId = 'plugin.video.hdrezka.tv'
